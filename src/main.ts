e8de7c0572c539e247d5d201a73e6dcb25d326f8
Jardel Bordignon <jardel1101@gmail.com>
2022-12-15 17:33:46 -0300

    Helmet, Cors e Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { useContainer } from 'class-validator'
+import helmet from 'helmet'
 
 import { AppModule } from './app/app.module'
 
@@ -9,9 +11,29 @@ const PORT = 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.use(helmet())
+  app.enableCors({ allowedHeaders: '*', origin: '*' })
+
   useContainer(app.select(AppModule), { fallbackOnErrors: true })
 
   app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+    .setTitle('Delivery')
+    .setDescription('The Delivery API Documentation')
+    .setVersion('0.1')
+    .build()
+
+  const swaggerOpts = {
+    swaggerOptions: {
+      apisSorter: 'alpha',
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  }
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('doc', app, document, swaggerOpts)
 
   const logger = new Logger('Bootstrap')
 
