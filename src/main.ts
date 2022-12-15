6a988f2c862ba40deb1a45af4ff86806e1fb2e8a
Jardel Bordignon <jardel1101@gmail.com>
2022-12-15 15:34:20 -0300

    Adicionado class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module'
+
+const PORT = 3000
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  const logger = new Logger('Bootstrap')
+
+  await app.listen(PORT).then(() => {
+    logger.log(`🚀 Server started on port ${PORT}`)
+  })
 }
-bootstrap();
+bootstrap()
