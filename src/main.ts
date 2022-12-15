f53882130140dc178f107c8894c4cd0993967fbb
Jardel Bordignon <jardel1101@gmail.com>
2022-12-15 15:54:38 -0300

    Config Prisma com Postgres

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { useContainer } from 'class-validator'
 
 import { AppModule } from './app.module'
 
@@ -7,6 +8,8 @@ const PORT = 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true })
 
   app.useGlobalPipes(new ValidationPipe())
 
